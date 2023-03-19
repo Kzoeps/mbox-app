889f0de9cce6476f012e0b4fab47180b82bf4916
kzoeps <kzoepa@gmail.com>
2023-03-19 18:17:34 -0400

    feat: update stuff

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
 import * as React from "react";
+import { useState,useEffect } from "react";
 import { WebView } from "react-native-webview";
+import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
+import { Camera } from 'expo-camera';
 import Constants from "expo-constants";
 
 export default function App() {
-  return (
-    <WebView
-      allowsInlineMediaPlayback={true}
-      mediaPlaybackRequiresUserAction={false}
-      style={styles.container}
-      source={{ uri: "https://mbox.kongtsey.com" }}
-    />
-  );
+  const [hasPermission, setHasPermission] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera, Please allow camera access</Text>;
+  }
+  if (hasPermission) {
+    return (
+      <WebView
+        allowsInlineMediaPlayback={true}
+        originWhitelist={["*"]}
+        javaScriptEnabled
+        scalesPageToFit
+        mediaPlaybackRequiresUserAction={false}
+        style={styles.container}
+        source={{ uri: "https://mbox.kongtsey.com" }}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
   },
 });
