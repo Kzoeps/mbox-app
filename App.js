0f4d5905509213fe8ed4edf71a6fc08b7889eae3
kzoeps <kzoepa@gmail.com>
2023-03-29 16:08:29 -0400

    feat: update stuff

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
 import * as React from "react";
-import { useState,useEffect } from "react";
+import { useEffect } from "react";
 import { WebView } from "react-native-webview";
-import { Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import { StyleSheet } from "react-native";
 import { Camera } from 'expo-camera';
-import Constants from "expo-constants";
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === "granted");
     })();
   }, []);
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera, Please allow camera access</Text>;
-  }
-  if (hasPermission) {
+  
     return (
+      <>
       <WebView
         allowsInlineMediaPlayback={true}
         originWhitelist={["*"]}
@@ -31,8 +23,9 @@ export default function App() {
         style={styles.container}
         source={{ uri: "https://mbox.kongtsey.com" }}
       />
+      <StatusBar backgroundColor={'black'} translucent={false}/>
+      </>
     );
-  }
 }
 
 const styles = StyleSheet.create({
